ff408135fbe98276ef49c2104b3a7863cb9631d3
Scott Wells <efwells101@gmail.com>
2019-05-23 12:34:52 -0600

    add header to layout; add logo file; change routing, add static serve

--- app.js
+++ app.js
@@ -10,28 +10,25 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Nerd Travel');
+    res.render('home');
 });
 
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Nerd Travel');
+    res.render('about');
 });
 
-// custom 404 page
-app.use(function(req, res){
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
     res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
-// custom 500 page
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
