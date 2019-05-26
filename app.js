e9402fee66376808dab9544f455a9fa2f319aa68
Scott Wells <efwells101@gmail.com>
2019-05-26 10:30:03 -0600

    added weather data to home page, weather data object, and context; added helper section for injecting into multiple views; added jquery test page

--- app.js
+++ app.js
@@ -1,25 +1,76 @@
+// add required files
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+// this engine displays templates as HTML in the browser
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    // helper section for injecting into multiple parts of the view
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    } 
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// app is viewed on localhost:3000
 app.set('port', process.env.PORT || 3000);
 
+// hides /public from the user; app only looks in the public folder
 app.use(express.static(__dirname + '/public'));
 
-// page testing
+// TDD page testing
 app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
 });
 
+// fake weather data
+// is handed to partials.weatherContext
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
 
-// routes
+// middleware to inject this data into res.locals.partials object
+// in this case, it gives partials.weatherContext the getWeatherData() function
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
+});
+
+// routing
 app.get('/', function(req, res){
     res.render('home');
 });
@@ -53,6 +104,7 @@ app.use(function(err, req, res, next){
     res.render('500');
 });
 
+// starts/stops the server
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
